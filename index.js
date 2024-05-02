59d509e4d50ca78d98dc9e0e3b6992e6fd46f075
taissir alem <taissir.alem@gmail.com>
2024-05-02 15:42:38 +0100

    checkpoint mongoose

---
+++ index.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+// const person = new Person({
+//     name: 'John Doe',
+//     age: 30,
+//     favoriteFoods: ['pizza', 'pasta'],
+//   });
+
+//   person.save((err, savedPerson) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+
+//     console.log('Person saved:', savedPerson);
+//   });
+
+// const arrayOfPeople = [
+//     { name: 'Alice', age: 25, favoriteFoods: ['sushi', 'ramen'] },
+//     { name: 'Bob', age: 40, favoriteFoods: ['steak', 'potatoes'] },
+//     { name: 'Charlie', age: 18, favoriteFoods: ['pizza', 'burgers'] },
+//   ];
+
+//   Person.create(arrayOfPeople, (err, createdPeople) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+
+//     console.log('People created:', createdPeople);
+//   });
+
+// Person.find({ name: 'John' }, (err, foundPeople) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+
+//     console.log('People found:', foundPeople);
+//   });
+
+// Person.findOne({ favoriteFoods: 'pizza' }, (err, foundPerson) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+
+//     console.log('Person found:', foundPerson);
+//   });
+
+// const personId = '_id';
+
+// Person.findById(personId, (err, foundPerson) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+
+//   console.log('Person found:', foundPerson);
+// });
